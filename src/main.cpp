fb38133b6952c7dade49067ce3451d2c6427d43c
Subhajit Sahu <wolfram77@gmail.com>
2016-08-31 18:39:52 +0530

    ospace: created

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,21 @@
 #include <stdio.h>
+#include "ospace.h"
 
+OSPACE_DECLARE(SInt, char)
+OSPACE_DEFINE(SInt, char)
 
 int main() {
+	SInt o;
+	char buffer[32];
+	printf("SInt_open(&o, buffer, 32).\n");
+	SInt_open(&o, buffer, 32);
+	printf("o.size: %d.\n", o.size);
+	SInt_openHeap(&o, 64);
+	printf("SInt_openHeap(&o, 64).\n");
+	SInt_resize(&o, 128);
+	printf("SInt_resize(&o, 128).\n");
+	printf("o.size: %d.\n", o.size);
+	SInt_close(&o);
+	printf("SInt_close(&o).");
 	return 0;
 }
---
+++ src/ospace.h
@@ -0,0 +1,110 @@
+#ifndef _OSPACE_H_
+#define _OSPACE_H_
+
+
+// Dhrtarastra said: O Sanjaya, what did my sons desirous of battle and the sons of Pandu do
+// after assembling at the holy land of righteousness, Kuruksetra?
+// ref: http://www.bhagavad-gita.org/Gita/verse-01-01.html
+
+
+#pragma region required headers
+#include <stddef.h>
+#include <stdlib.h>
+#pragma endregion
+
+
+
+#pragma region type
+// Define structure for space.
+// args: name, type.
+#define OSPACE_TYPE(N, T) \
+	typedef struct _##N { \
+		T* at; \
+		size_t size; \
+	}N
+#pragma endregion
+
+
+
+#pragma region function
+// Open space with static memory.
+// args: space, at, size.
+#define OSPACE_FD_OPEN(N, T) \
+	inline T* N##_open(void* o, void* at, size_t size)
+
+#define OSPACE_FB_OPEN(N, T) \
+	OSPACE_FD_OPEN(N, T) { \
+		N* z = (N*)o; \
+		z->at = (T*)at; \
+		z->size = size; \
+		return z->at; \
+	}
+
+
+// Open space with heap memory.
+// args: space, size.
+#define OSPACE_FD_OPENHEAP(N, T) \
+	inline T* N##_openHeap(void* o, size_t size)
+
+#define OSPACE_FB_OPENHEAP(N, T) \
+	OSPACE_FD_OPENHEAP(N, T) { \
+		N* z = (N*)o; \
+		z->at = (T*)malloc(size * sizeof(T)); \
+		z->size = z->at? size : 0; \
+		return z->at; \
+	}
+
+
+// Close space opened with heap memory.
+// args: space.
+#define OSPACE_FD_CLOSE(N, T) \
+	inline T* N##_close(void* o)
+
+#define OSPACE_FB_CLOSE(N, T) \
+	OSPACE_FD_CLOSE(N, T) { \
+		N* z = (N*)o; \
+		free(z->at); \
+		return NULL; \
+	}
+
+
+// Resize space, if opened with heap memory.
+// args: space, size.
+#define OSPACE_FD_RESIZE(N, T) \
+	inline T* N##_resize(void* o, size_t size)
+
+#define OSPACE_FB_RESIZE(N, T) \
+	OSPACE_FD_RESIZE(N, T) { \
+		N* z = (N*)o; \
+		z->at = (T*)realloc(z->at, size * sizeof(T)); \
+		z->size = z->at ? size : 0; \
+		return z->at; \
+	}
+#pragma endregion
+
+
+
+#pragma region macro
+// Declare a space type.
+// This defines structure and declares functions.
+// args: name, type.
+#define OSPACE_DECLARE(N, T) \
+	OSPACE_TYPE(N, T); \
+	OSPACE_FD_OPEN(N, T); \
+	OSPACE_FD_OPENHEAP(N, T); \
+	OSPACE_FD_CLOSE(N, T); \
+	OSPACE_FD_RESIZE(N, T);
+
+
+// Define a space type.
+// This define functions.
+// args: name, type.
+#define OSPACE_DEFINE(N, T) \
+	OSPACE_FB_OPEN(N, T) \
+	OSPACE_FB_OPENHEAP(N, T) \
+	OSPACE_FB_CLOSE(N, T) \
+	OSPACE_FB_RESIZE(N, T)
+#pragma endregion
+
+
+#endif // !_OSPACE_H_
